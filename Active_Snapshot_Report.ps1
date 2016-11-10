20eec9062156f7c1bfc19bb3fd54e1392a927aec
Scripting Cactus <harryj.butler@googlemail.com>
2016-11-10 15:09:33 +0000

    Version 1.1.0 Changed script to use standard template Removed reliance for stored credentials in a file

--- Active_Snapshot_Report.ps1
+++ Active_Snapshot_Report.ps1
@@ -1,50 +1,212 @@
-######################################################################################################
-# Author: Scripting-Cactus
-# Date : 09/11/2016
-# Version: 1.0.0
-######################################################################################################
-# Only edit items in this section otherwise the script may not work. Read the readme.md for more info.
-#
-$vcenters = @("vCenter-1", "vCenter-2", "etc")
-#
-######################################################################################################
-try
-    {
-        Add-PSSnapin VMware.VimAutomation.Core1 -ErrorAction Stop
+#requires -version 2
+<#
+.SYNOPSIS
+    This script is designed to return a list of all active snapshots
+  
+.DESCRIPTION
+    This script is designed to return a list of all active snapshots
+  
+.PARAMETER $Vcenter_Array
+    Creates an array of the vcenter servers you want to run this script against
+
+.PARAMETER $Credential_User
+    Specifiy the user to connect to the vCenter servers 
+	as
+.INPUTS
+    Credential XML files need to be stored in folder relative to the script in \CredentialStore
+    Credential files need to be named 
+  
+.OUTPUTS
+    Log file stored in folder relative to the script in \Output\Vmware_Active_Snapshots_Report.log
+  
+.NOTES
+    Version:        1.1.0
+    Author:         Scripting-Cactus
+    Creation Date:  10/11/2016
+    Purpose/Change: Changed script to use standard template
+                    Removed reliance for stored credentials in a file
+  
+.EXAMPLE
+    $Vcenter_Array = @("vCenter-1", "vCenter-2", "etc")
+#>
+
+#----------------------------------------------------------[Declarations]----------------------------------------------------------
+
+#Script Version
+$Project_Author = "Scripting-Cactus"
+$Project_Name = "VMware-Active-Snapshot-Report"
+$Local_Version = "Version 1.1.0"
+
+#Path Decloration
+$Invocation = (Get-Variable MyInvocation).Value
+$Directory_Path = Split-Path $Invocation.MyCommand.Path
+
+#Log File Info
+$Log_Path = $Directory_Path + "\Output"
+$Log_Name = $Project_Name + ".log"
+$Log_File = Join-Path -Path $Log_Path -ChildPath $Log_Name
+$Output_Name = "Active_Snapshots.csv"
+$Output_File = Join-Path -Path $Log_Path -ChildPath $Output_Name
+
+#Array of vCenter servers to connect to
+$Vcenter_Array = @("vCenter-1")
+
+# Set user to run script as
+$Credentials_User = "username"
+
+#---------------------------------------------------------[Initialisations]--------------------------------------------------------
+
+# Start Logging
+Add-content $Log_File -value "$Project_Name"
+Add-content $Log_File -value "$(Get-Date) : Initialising Script"
+write-host "Initialising Script"
+
+#Add VMware PowerCLI Snap-Ins
+try{
+	Add-PSSnapin VMware.VimAutomation.Core -ErrorAction Stop
+}
+catch{
+	write-host "VMware Snapin could not be loaded. Is VMware PowerCli installed?"
+	Add-content $Log_File -value "$Log_Date : $_"
+	break
+}
+Finally{
+	Add-content $Log_File -value  "$(Get-Date) : VMware Snapin successfully loaded"
+	write-host "VMware Snapin successfully loaded"
+}
+
+# Set Error Preferences
+$WarningActionPreference = "SilentlyContinue"
+$ErrorActionPreference = "Stop"
+
+# Create snapshot csv file and header
+try{
+	New-Item -path $Output_File -type file -force
+	Add-content $Output_File -value '"snapVC", "snapEventVM", "snapEventInfo", "snapEventDate", "snapEventUser"'
+	$snapshot = "" | select-object snapVC, snapEventVM, snapEventInfo, snapEventDate, snapEventUser
+	write-host "CSV file created"
+	Add-content $Log_File -value "$(Get-Date) : CSV file created at $$Output_File"
+}
+catch{
+	write-host "CSV file could not be created. Are the permissions set correctly?"
+	Add-content $Log_File -value "$(Get-Date) : $_"
+	break
+}
+
+#-----------------------------------------------------------[Functions]------------------------------------------------------------
+
+#Function to connect to vCenters
+Function Connect-VMwareServer{
+    Param([Parameter(Mandatory=$true)][string]$Vcenter_Server)
+    Begin{
+            Add-content $Log_File -value "$(Get-Date) : Attempting to connect to $Vcenter_Server"
     }
-catch
-    {
-        write-host "VMware Snapin could not be loaded. Is VMware powercli installed?"
-        break
+    Process{
+            Try{
+				$Credentials = Get-VICredentialStoreItem -Host $Vcenter_Server | where {$_.User -eq $Credentials_User}
+				Connect-VIServer $Vcenter_Server -User $Credentials.User -Password $Credentials.Password
+            }
+            Catch{
+				Add-content $Log_File -value "$(Get-Date) : $_"
+				write-host "Error connecting to vCenter $Vcenter_Server. See log file $Log_File for details."
+				$Connected = "False"
+				$Errors_Encounterd = "True"
+                return
+            }
     }
-$invocation = (Get-Variable MyInvocation).Value
-$directorypath = Split-Path $invocation.MyCommand.Path 
-New-Item $directorypath + "\Output\Active_Snapshots.csv" -type file -force
-$snapshot = "" | Select-Object snapVC, snapEventVM, snapEventInfo, snapEventDate, snapEventUser
-foreach($vCenter in $vCenters)
-	{
-		$vCenterCreds = $directorypath + "\CredentialStore\"+$vcenter +"_Credentials.xml"
-        $Creds = Get-VICredentialStoreItem -Host $vcenter -File $vCenterCreds
-        Connect-VIServer $vcenter -User $Creds.User -Password $Creds.Password
+    End{
+            If($Connected -eq "True"){
+                Add-content $Log_File -value  "$(Get-Date) : Successfully connected to $Vcenter_Server"
+                write-host "Successfully connected to $Vcenter_Server"
+            }
+    }
+}
+
+#Function to connect to vCenters
+Function Disconnect-VMwareServer{
+    Param([Parameter(Mandatory=$true)][string]$Vcenter_Server)
+    Begin{
+            Add-content $Log_File -value "$(Get-Date) : Attempting to Disconnect from $Vcenter_Server"
+    }
+    Process{
+		$Connection_State = $defaultviserver | foreach {$_.IsConnected}
+        if($Connection_State -eq "True"){
+            disconnect-VIServer -server $Vcenter_Server -confirm:$false
+            $Disconnected = "True"
+        }
+        else{
+            Add-content $Log_File -value  "$(Get-Date) : Cannot disconnect from $Vcenter_Server as it is not connected"
+            write-host "Cannot disconnect from $Vcenter_Server as it is not connected"
+			$Errors_Encounterd = "True"
+            return
+        }
+    }
+    End{
+        If($Disconnected -eq "True"){
+            Add-content $Log_File -value  "$(Get-Date) : Successfully disconnected from $Vcenter_Server"
+            write-host "Successfully disconnected from $Vcenter_Server"
+        }
+    }
+}
+
+# Function to list active snapshots
+Function List-Active_Sanpshots{
+	Add-content $Log_File -value  "$(Get-Date) : Retrieving list of active snapshots"
+    write-host "Retrieving list of active snapshots"
+	try{
 		Get-VM | Get-Snapshot |%{
-			$snap = $_
-			$snapevent = Get-VIEvent -Entity $snap.VM -Types Info -Finish $snap.Created -MaxSamples 1 | Where-Object {$_.FullFormattedMessage -imatch 'Task: Create virtual machine snapshot'}
-			$snapshot.snapVC = $vCenter
-			if ($snapevent -ne $null)
-				{ 
-					$snapshot.snapEventVM = $snap.VM
-					$snapshot.snapEventInfo = $snap
-					$snapshot.snapEventDate = $snap.Created.DateTime
-					$snapshot.snapEventUser = $snapevent.UserName
-				}
-			else 
-				{
-					$snapshot.snapEventVM = $snap.VM
-					$snapshot.snapEventInfo = $snap
-					$snapshot.snapEventDate = ("Unknown")
-					$snapshot.snapEventUser = ("This event is not in vCenter events database")
-				}
-			$snapshot
-		} | Export-Csv -Path  $directorypath + "\Output\Active_Snapshots.csv" -NoTypeInformation -UseCulture -Force -Append
-        Disconnect-VIServer -Server $vcenter -confirm:$false
+				$snap = $_
+				$snapevent = Get-VIEvent -Entity $snap.VM -Types Info -Finish $snap.Created -MaxSamples 1 | Where-Object {$_.FullFormattedMessage -imatch 'Task: Create virtual machine snapshot'}
+				$snapshot.snapVC = $vCenter
+				if ($snapevent -ne $null)
+					{ 
+						$snapshot.snapEventVM = $snap.VM
+						$snapshot.snapEventInfo = $snap
+						$snapshot.snapEventDate = $snap.Created.DateTime
+						$snapshot.snapEventUser = $snapevent.UserName
+					}
+				else 
+					{
+						$snapshot.snapEventVM = $snap.VM
+						$snapshot.snapEventInfo = $snap
+						$snapshot.snapEventDate = ("Unknown")
+						$snapshot.snapEventUser = ("This event is not in vCenter events database")
+					}
+				$snapshot
+			} | Export-Csv -Path $Output_File -NoTypeInformation -UseCulture -Force -Append
+	}
+	catch{
+		Add-content $Log_File -value "$(Get-Date) : $_"
+		write-host "Error retrieving running snapshots for $vCenter. See log file $Log_File for details."
+		$Errors_Encounterd = "True"
+		return
+	}
+	finally{
+        If($Errors_Encounterd -ne "True"){
+			Add-content $Log_File -value  "$(Get-Date) : Successfully retrieved list of active snapshots"
+			write-host "Successfully retrieved list of active snapshots"
+		}	
+	}
+}
+
+#-----------------------------------------------------------[Execution]------------------------------------------------------------
+
+foreach($vCenter in $vCenter_Array)
+	{
+        Connect-VMwareServer -Vcenter_Server $Vcenter
+        List-Active_Sanpshots
+        Disconnect-VMwareServer $Vcenter
     }
+
+#-----------------------------------------------------------[Finalisation]---------------------------------------------------------
+
+if($Errors_Encounterd = "True")
+	{
+		Add-content $Log_File -value "$(Get-Date) : Script has completed with errors"
+		write-host "Script has completed with errors. Please review the log file located at $Log_File"
+	}
+else
+	{
+		Add-content $Log_File -value "$(Get-Date) : Script has been successfully completed"
+		write-host "Script has been successfully completed"
+	}
--- readme.md
+++ readme.md
@@ -1,14 +1,14 @@
 # VMware Active Snapshot Report
 This script is designed to run against multiple VMware vCenter servers and return a list of all active snapshots.
+The are exmaples of the log file and the output in the output folder.
 
 ## Prerequistes
 
 ### Software
-For this script to be able to run it is required that VMware powercli is installed so that powershell can talk to the vCenters and run the required commands.
+For this script to be able to run it is required that VMware Powercli is installed so that Powershell can talk to the vCenters and run the required commands.
 
 ### Stored Credentials
-This script is based on the VICredentialStoreItem xml file method of authentication. You will need ot create an xml file and store it in the \CredentailStore folder for the script to work. See https://www.vmware.com/support/developer/windowstoolkit/wintk40u1/html/New-VICredentialStoreItem.html for an example on how to creat your file. There is a file included in the \CredentialStore folder to give you an idea of what the xml file output will look like. This will need to be done for every vCenter you want to connect to and requires a seperate xml file for each.
-The xml files will need to be located in the \CredentialStore folder and named in the following way: vCenterName_Credentials.xml e.g. vCenter-1.local_Credentials.xml
+This script is based on the VICredentialStoreItem method of authentication. You will need to create some credentials using New-VICredentialStoreItem See https://www.vmware.com/support/developer/windowstoolkit/wintk40u1/html/New-VICredentialStoreItem.html for an example on how to create your credentials.
 
 ## Configuring the Script
-To get this script running all you need to do is replace the data in the vCenters array with the FQDN or IP address of your vCenter Servers. Make sure that teh names you place in the array match the start of the credentials xml file name otherwise it will not be able to connect.
+To get this script running all you need to do is replace the data in the $Venters_Array with the FQDN or IP address of your vCenter Servers. this will work with a single vcenter in the array or multiple.
